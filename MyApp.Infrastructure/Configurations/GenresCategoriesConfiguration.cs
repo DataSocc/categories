98c1300d3426d15adac266455606f161896ee1a4
DataSocc <163157985+DataSocc@users.noreply.github.com>
2024-05-12 02:07:40 +0000

    migrations

--- MyApp.Infrastructure/Configurations/GenresCategoriesConfiguration.cs
+++ MyApp.Infrastructure/Configurations/GenresCategoriesConfiguration.cs
@@ -7,9 +7,23 @@ internal class GenresCategoriesConfiguration
     : IEntityTypeConfiguration<GenresCategories>
 {
     public void Configure(EntityTypeBuilder<GenresCategories> builder)
-        => builder.HasKey(relation => new
+    {
+        builder.HasKey(relation => new
         {
             relation.CategoryId,
             relation.GenreId
         });
+
+        // Configuração para exclusão em cascata no relacionamento com Genre
+        builder.HasOne(relation => relation.Genre)
+               .WithMany() // Assuma que Genre tem uma coleção de GenresCategories, se existir
+               .HasForeignKey(relation => relation.GenreId)
+               .OnDelete(DeleteBehavior.Cascade); // Exclusão em cascata
+
+        // Configuração para exclusão em cascata no relacionamento com Category
+        builder.HasOne(relation => relation.Category)
+               .WithMany() // Assuma que Category tem uma coleção de GenresCategories, se existir
+               .HasForeignKey(relation => relation.CategoryId)
+               .OnDelete(DeleteBehavior.Cascade); // Exclusão em cascata
+    }
 }
